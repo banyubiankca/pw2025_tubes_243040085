bd5879fb0102eafe7b8452f6e55f3a8251c0c9dc
banyubiankca <banyubiankca13@gmail.com>
2025-05-20 13:51:40 +0700

    init

--- index.php
+++ index.php
@@ -5,10 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
-<h1>HEllO WORLD!!!</h1>
-
 <body>
+
+<h1>Hello World!</h1>
     
 </body>
 </html>
